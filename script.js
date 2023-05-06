f0cfb0439a6c593f83ea3f5a070ec7825be4460a
raluvy95 <ralucadoge069@gmail.com>
2023-05-06 16:20:32 +0300

    multiwindow worked, but close button don't work for some reasons

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-$(".window").draggable()
+const array_of_windows = []
 
-// multiwindow would work with array of windows
-$(".template").clone().appendTo("body").draggable()
+$(document).ready(() => {
+    for (let i = 0; i < 10; i++) {
+        const id = Date.now().toString()
+        array_of_windows.push({
+            id,
+            html: `<div class="window template">
+        <div class="title">
+            <div class="actual-title">
+                EPIC
+            </div>
+            <div class="window-action">
+                <p class="close" onclick='close("${id}")'>X</p>
+            </div>
+        </div>
+        <div class="window-content">
+            <h1>LOL</h1>
+        </div>
+    </div>`
+        })
+    }
+
+    reload()
+    $(".window").draggable()
+})
+
+function reload() {
+    $("#windows").empty()
+    for (const window of array_of_windows) {
+        $("#windows").append(window.html)
+    }
+}
+
+function close(id) {
+    console.log(`does it actually work ${id}`)
+    // array_of_windows.splice(array_of_windows.indexOf(e => e.id === id), 1)
+    // reload()
+}
+// // $(".close").on('click', close(id))
+// function close(id) {
+//     console.log("does it actually work")
+//     array_of_windows.splice(array_of_windows.indexOf(e => e.id === id), 1)
+//     reload()
+// }
