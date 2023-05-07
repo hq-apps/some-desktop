bfbab6353d686bd2a008de802b5610fedf1ebc38
E-Codz <iamecodzz@proton.me>
2023-05-07 15:33:15 +0200

    window shadows

--- src/Window.svelte
+++ src/Window.svelte
@@ -232,6 +232,14 @@
         font-family: "Inter", sans-serif;
         border-radius: 15px;
         overflow: hidden;
+        box-shadow:
+            0px 6.4px 13.8px rgba(0, 0, 0, 0.02),
+            0px 12.9px 24.1px rgba(0, 0, 0, 0.028),
+            0px 20px 31.6px rgba(0, 0, 0, 0.035),
+            0px 29.5px 38px rgba(0, 0, 0, 0.042),
+            0px 46.4px 46.9px rgba(0, 0, 0, 0.05),
+            0px 100px 80px rgba(0, 0, 0, 0.07)
+        ;
     }
     .title {
         display: flex;
