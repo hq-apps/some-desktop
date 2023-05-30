87d143517ff38eca25d2e836ac1e47859a15a375
E-Codz <iamecodzz@proton.me>
2023-05-30 12:06:06 +0200

    info

--- src/contents/About.svelte
+++ src/contents/About.svelte
@@ -1,8 +1,31 @@
+<script>
+    import { read, write } from "../lib/PersistantStorage";
+
+    function makeid(length) {
+        var result = "";
+        var characters =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+            );
+        }
+        return result;
+    }
+
+    const serial =
+        read("mecos", "serialnumber") || write("mecos", "serialnumber", makeid(4)+"CUM"+makeid(5));
+</script>
+
 <h1>MEC OS XXX Compooper</h1>
 <pre>
-    CPU:  Intol Permtium 54 Ghz
-    RAM: 99 MB
-    GPU: Novidiu 30214 rtx
+    <b>iCwoud PC</b>
+    CPU: 42x Intol sPermtium 6,9 Ghz
+    RAM: -1 MB
+    GPU: NoVidiu DiVORCE 6090 rtx 13gb
+    Startup Disk: /homework
+    Serial Number: {serial}
 </pre>
 <iframe
     width="853"
--- src/lib/PersistantStorage.ts
+++ src/lib/PersistantStorage.ts
@@ -1,5 +1,6 @@
 export function write(app: string, key: string, value: string) {
     window.localStorage.setItem(`${app}-${key}`, value)
+    return value
 }
 
 export function read(app: string, key: string) {
